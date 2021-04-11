493a04ec3d7e98113563deb2d2fbe33283666ebe
Javier Gutiérrez <ggutierrez.javier@gmail.com>
2021-04-11 13:47:50 -0400

    Initial commit

---
+++ controllers/image.js
@@ -0,0 +1,31 @@
+const Clarifai = require('clarifai');
+const { response } = require('express');
+
+const app = new Clarifai.App({
+    apiKey: '76414973e78d4859945c97aafc6f3756'
+});
+
+const handleApiCall = (req, res) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('unable to work with Api'))
+}
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('user').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
