58b9a6c193f1ab2f745c003f551ca92a53e45770
Javier Gutiérrez <ggutierrez.javier@gmail.com>
2021-04-12 13:04:51 -0400

    Add DB table fix

--- controllers/image.js
+++ controllers/image.js
@@ -16,7 +16,7 @@ const handleApiCall = (req, res) => {
 
 const handleImage = (req, res, db) => {
     const { id } = req.body;
-    db('user').where('id', '=', id)
+    db('users').where('id', '=', id)
     .increment('entries', 1)
     .returning('entries')
     .then(entries => {
--- controllers/signin.js
+++ controllers/signin.js
@@ -8,7 +8,7 @@ const handleSignin = (db, bcrypt) => (req, res) => {
         .then(data => {
             const isValid = bcrypt.compareSync(password, data[0].hash);
             if (isValid) {
-                return db.select('*').from('user')
+                return db.select('*').from('users')
                     .where('email', '=', email)
                     .then(user => {
                         res.json(user[0]);
