1d53d4c02ac7be6f74dfe56a0f9755be0b4beac5
Javier Gutiérrez <ggutierrez.javier@gmail.com>
2021-04-12 12:30:42 -0400

    Add minor change

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
     client: 'pg',
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: true
     }
 })
 
@@ -23,7 +23,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send('success') })
+app.get('/', (req, res) => { res.send(`success ${process.env}`) })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
