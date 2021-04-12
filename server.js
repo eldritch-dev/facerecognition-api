bd8cca82e680474e315c0b7d0f16bb6a36e0fe61
Javier Gutiérrez <ggutierrez.javier@gmail.com>
2021-04-12 08:50:26 -0400

    Add minor fix

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send('success' + db.connection.connectionString.DATABASE_URL) })
+app.get('/', (req, res) => { res.send('success' + db.connection.connectionString) })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
